5c64282ac0f75e0e86ce5df2d08504cc198e8aaf
Abubackar Siddiq <abubackarsiddiqs.24csd@kongu.edu>
2026-07-07 23:08:05 +0530

    Time: 2 ms (72.23%), Space: 41.9 MB (99.9%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        int count = 0, top = 0;
+        int top = 0;
         for (int i = 0; i < t.length() && top < s.length(); i++) {
             if (t.charAt(i) == s.charAt(top)) {
-                count++;
                 top++;
             }
         }
-        return count == s.length() ? true : false;
+        return top == s.length() ? true : false;
     }
 }
